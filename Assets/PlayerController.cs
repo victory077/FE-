853f4e005f8b65a9bffe555c67321705e65b4c80
miyazaki <victory077@i.softbank.jp>
2015-06-06 19:52:01 +0900

    現在開発中

---
+++ Assets/PlayerController.cs
@@ -0,0 +1,58 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PlayerController : MonoBehaviour 
+{
+	public int Movable;
+	// Use this for initialization
+	void Start () 
+	{
+		PlayerMove ();
+	}
+	
+	// Update is called once per frame
+	void Update () 
+	{
+
+	}
+	public void PlayerMove ()
+	{
+		Movable = PublicStatic.PMovable;
+	}
+	public void TurnEnemy ()
+	{
+		this.GetComponent<PlayerController> ().enabled = false;
+	}
+	public void PlayerUp ()
+	{
+		if (Movable > 0)
+		{
+			this.transform.position += new Vector3 (0f,0f,1f);
+			Movable --;
+		}
+	}
+	public void PlayerDown ()
+	{
+		if (Movable > 0)
+		{
+			this.transform.position += new Vector3 (0f,0f,-1f);
+			Movable --;
+		}
+	}
+	public void PlayerRight ()
+	{
+		if (Movable > 0)
+			{
+			this.transform.position += new Vector3 (1f,0f,0f);
+			Movable --;
+		}
+	}
+	public void PlayerLeft ()
+	{
+		if (Movable > 0)
+		{
+			this.transform.position += new Vector3 (-1f,0f,0f);
+			Movable --;
+		}
+	}
+}
---
+++ Assets/PublicStatic.cs
@@ -0,0 +1,17 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PublicStatic : MonoBehaviour {
+	public static int PMovable;
+	public static int PPower;
+	public static int PDefence;
+	// Use this for initialization
+	void Awake () {
+		PublicStatic.PMovable = 3;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+
+	}
+}
